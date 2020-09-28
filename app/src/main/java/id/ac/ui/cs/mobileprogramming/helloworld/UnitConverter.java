5a03cf2942b0e931295b107a2189741b305ab6e2
Samuel Dimas <samueldimas543@gmail.com>
2020-09-28 23:48:58 +0700

    Lab 2 Tasks

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/UnitConverter.java
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.mobileprogramming.helloworld;
+
+public class UnitConverter {
+    public static int convertKilogramToGram(int input) {
+        int result = input * 1000;
+        return result;
+    }
+}
